bb06da70ac59e4aa815220fa5548073119eb9ad0
Rajanm105 <rajanmishra96034@gmail.com>
2025-06-04 12:25:19 +0530

    todo app completed

--- todoapp/lib/main.dart
+++ todoapp/lib/main.dart
@@ -1,125 +1,213 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+    
+  // Entry point of the app
+  runApp(MyApp()); 
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        
+      // Sets the home screen of the app
+      home: HomeScreen(), 
+      
+      // Removes debug banner
+      debugShowCheckedModeBanner: false, 
+      
+      // Sets the app theme color
+      theme: ThemeData(primarySwatch: Colors.indigo), 
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenState extends State<HomeScreen> {
+  
+  // List to store tasks
+  List<String> todoList = []; 
+  
+  // Controller for text input
+  final TextEditingController _controller = TextEditingController(); 
+  
+  // Index to track which task is being edited
+  int updateIndex = -1; 
 
-  void _incrementCounter() {
+  // Function to add a new task to the list
+  addList(String task) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      todoList.add(task);
+      _controller.clear();
+    });
+  }
+
+  // Function to update an existing task
+  updateListItem(String task, int index) {
+    setState(() {
+      todoList[index] = task;
+      
+      // Reset update index
+      updateIndex = -1; 
+      _controller.clear();
+    });
+  }
+
+  // Function to delete a task
+  deleteItem(index) {
+    setState(() {
+      todoList.removeAt(index);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(
+          "Todo Application",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+          ),
+        ),
+        
+        // Centers the app bar title
+        centerTitle: true, 
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        margin: EdgeInsets.all(10),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Expanded(
+              flex: 90,
+              child: ListView.builder(
+                  
+                  // Number of tasks in the list
+                  itemCount: todoList.length, 
+                  itemBuilder: (context, index) {
+                    return Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      
+                      // Card background color
+                      color: Colors.green, 
+                      child: Container(
+                        margin: EdgeInsets.only(left: 20),
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.all(10),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 80,
+                              child: Text(
+                                  
+                                // Display the task text
+                                todoList[index], 
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20),
+                              ),
+                            ),
+                            
+                            // Edit button
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _controller.clear();
+                                  _controller.text = todoList[index];
+                                  updateIndex = index;
+                                });
+                              },
+                              icon: Icon(
+                                Icons.edit,
+                                size: 30,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            
+                            // Delete button
+                            IconButton(
+                              onPressed: () {
+                                deleteItem(index);
+                              },
+                              icon: Icon(
+                                Icons.delete,
+                                size: 30,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Expanded(
+                flex: 10,
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 70,
+                      child: SizedBox(
+                        height: 60,
+                        child: TextFormField(
+                            
+                          // Input field controller
+                          controller: _controller, 
+                          decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(
+                                  color: Colors.green,
+                                )),
+                            filled: true,
+                            
+                            // Placeholder text
+                            labelText: 'Create Task....', 
+                            labelStyle: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                    
+                    // Floating action button for adding/updating tasks
+                    FloatingActionButton(
+                      backgroundColor: Colors.green,
+                      foregroundColor: Colors.white,
+                      onPressed: () {
+                        updateIndex != -1
+                            ? updateListItem(_controller.text,
+                                updateIndex) // Update task if editing
+                            : addList(_controller.text); // Add new task
+                      },
+                      child: Icon(updateIndex != -1
+                          ? Icons.edit
+                          : Icons.add), // Icon changes based on action
+                    ),
+                  ],
+                )),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
